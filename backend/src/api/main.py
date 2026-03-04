496ebb51291db9d5a1b0595489b6de6e489d641d
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-04 16:38:38 -0300

    feat: register middleware, exceptions, coneciton, cors (#81)

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,8 +1,13 @@
 from contextlib import asynccontextmanager
-
-from fastapi import FastAPI, Depends
-from infrastructure.database.connection import (create_engine,create_session_factory,dispose_engine)
+from fastapi import FastAPI
 from api.routers.user import router as users
+from api.middlewares.exception_handler import (business_exception_handler, validation_exception_handler, domain_exception_handler)
+from domain.exceptions.business_exception import BusinessException
+from domain.exceptions.validation_exception import ValidationException
+from domain.exceptions.domain_exception import DomainException
+from infrastructure.database.connection import (create_engine, create_session_factory, dispose_engine)
+from infrastructure.config.cors import setup_cors
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -22,4 +27,10 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+setup_cors(app)
+
 app.include_router(users)
+
+app.add_exception_handler(BusinessException, business_exception_handler)
+app.add_exception_handler(ValidationException, validation_exception_handler)
+app.add_exception_handler(DomainException, domain_exception_handler)

d639b1e91ace980a7dd4d35a6f1634da587467f1
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-04 16:54:38 -0300

    feat: models (#86)

---
+++ backend/src/infrastructure/database/models/user_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, Date
+from uuid import uuid4
+from datetime import date
+from infrastructure.database.base import Base
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name: Mapped[str] = mapped_column(String(100))
+    lastname: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password_hash: Mapped[str] = mapped_column(String(255))
+    birthdate: Mapped[date]
+    gender: Mapped[str] = mapped_column(String(20))
+    profession: Mapped[str] = mapped_column(String(100))
+    phone: Mapped[str] = mapped_column(String(20))

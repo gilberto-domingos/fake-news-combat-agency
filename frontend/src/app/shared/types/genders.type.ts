07d9794f92de2d15d13c93e065216f56527ee585
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-05 21:08:16 -0300

    refactor: move frontend to separate repository (#102)

--- frontend/src/app/shared/types/genders.type.ts
+++
@@ -1,9 +0,0 @@
-export const Genders = {
-  male: 'Masculino',
-  female: 'Feminino',
-  non_binary: 'Não binário',
-  transgender: 'Transgênero',
-  other: 'Outro',
-} as const;
-
-export type Gender = (typeof Genders)[keyof typeof Genders];

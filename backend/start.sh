2ba583bf6e7717a4de202d754f4b9714f82c586a
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-08 00:45:12 -0300

    fix port uvicorn (#123)

--- backend/start.sh
+++ backend/start.sh
@@ -2,9 +2,9 @@
 
 echo "Starting application..."
 
-uvicorn backend.src.api.main:app \
+exec uvicorn src.api.main:app \
   --host 0.0.0.0 \
-  --port $PORT \
+  --port ${PORT:-8000} \
   --workers 2
 #  workers = (CPU * 2) + 1
 

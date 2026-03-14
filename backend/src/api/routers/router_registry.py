6c8ae1a24fb6c76caee6088f7d50596b85e13642
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-14 19:51:10 -0300

    feat: register auth_router (#147)

--- backend/src/api/routers/router_registry.py
+++ backend/src/api/routers/router_registry.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from src.api.routers.user_router import router as users
+from src.api.routers.auth_router import router as auth_router
 from src.api.routers.user_router import router as counter_access
 
 api_router = APIRouter()
 
 api_router.include_router(users)
+api_router.include_router(auth_router)
 api_router.include_router(counter_access)

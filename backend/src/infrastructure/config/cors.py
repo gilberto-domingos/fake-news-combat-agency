398266beedef1d700a99c8486fc0a50ccc271fdc
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-03-15 01:11:21 -0300

    chore: new cors (#149)

--- backend/src/infrastructure/config/cors.py
+++ backend/src/infrastructure/config/cors.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     origins = [
         "http://localhost:4200",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
         "https://b1010code.com.br",
     ]
 

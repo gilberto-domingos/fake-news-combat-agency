48876ed9367d78cef16ef4e9a87d233a17a004ce
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-05-25 15:37:42 -0300

    chore: connection.py database url (#177)

--- backend/src/infrastructure/database/connection.py
+++ backend/src/infrastructure/database/connection.py
@@ -1,4 +1,3 @@
-# src/infrastructure/database/connection.py
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -14,10 +13,21 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL NOT DEFINED")
 
-DATABASE_URL = DATABASE_URL.replace(
-    "postgres://",
-    "postgresql+asyncpg://"
-)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+asyncpg://",
+        1
+    )
+
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1
+    )
+
+    print("DATABASE_URL:", DATABASE_URL)
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession, async_sessionmaker
 
